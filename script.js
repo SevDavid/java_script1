0d95ce2b2575031aaf3de4d16c955229c88da7ca
SevDavid <115550375+SevDavid@users.noreply.github.com>
2022-12-10 12:35:48 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let nome="David";
+console.log(nome)
+let idade = 18;
+console.log(18)
+console.log(18*7)
+let amigos =[
+    {
+        nome: "José",
+        idade: 18,
+        éestudante: true
+    },
+    {
+        nome: "André",
+        idade: 19,
+        éestudante: false
+    },
+    {
+        nome: "Pedro",
+        idade: 20,
+        éestudante: true
+    }
+];
+console.log(amigos)
+
+let caract =[
+    {
+        nome:"José",
+        caract:"main Frânces/ main Carro/ main Desenho",  
+    },
+    {
+        nome:"André",
+        caract:"main Jazz/ main Música/ main Piano ",
+    },
+    {
+        nome:"Pedro",
+        caract:"main Programação/ main Cultura/ Vocabulário",
+    },
+]
+console.log(caract)
+
